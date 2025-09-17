e866e358ac6ac591108cf6018b102261699c3413
Dejan Pantelić <dejannrt411@gmail.com>
2025-09-17 10:03:35 +0200

    Added info for search

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const corsProxy = 'https://api.codetabs.com/v1/proxy?quest=';
 export const cobbisUrl = 'https://plus.cobiss.net/cobiss/sr/sr';
 
 export const searchErrorMessage = 'Došlo je do greške prilikom pretrage. Molimo pokušajte ponovo kasnije.'
-export const textForMain = `Unesi izdavaca i saznaj sta izlazi ${new Date().getFullYear()} godine :)`;
+export const textForMain = `Unesi izdavača i saznaj šta izlazi ${new Date().getFullYear()} godine :)`;
